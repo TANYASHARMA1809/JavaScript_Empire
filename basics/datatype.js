3656be4eb26da3de1307405d5bc4bbff732ae1a2
Tanya <tannusharma.dk@gmail.com>
2024-08-01 00:11:27 +0530

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,14 @@
+"use strict"; //traet all JS code as newer version
+
+// alert("hello")..gives error bcoz we are using node js not browser
+
+//number=> 2 to power 53
+//string
+//bigint
+//string =""
+//null=standalone value,user gives herself
+//undefined=browser reflect //symbol= unique
+
+
+//object
+console.log(typeof null);//object
---
+++ basics/variable.js
@@ -0,0 +1,6 @@
+let x=10;//block scope
+const mail="tannu.dk@gmail.com"//cannot change
+var city="mathura"//not used now..bcoz of issue of block scope and functional scope
+//{}..this is scope
+
+console.table([x,mail,city]);
