febb7b1562d912409953de63bebde76b02e2d14a
Tanya <tannusharma.dk@gmail.com>
2024-08-01 17:30:45 +0530

    datattypes conversion

---
+++ basics/3conversionOperation.js
@@ -0,0 +1,31 @@
+// let score=null;
+// console.log(typeof score);
+
+// let valueNumber=Number(score);
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+//"33abc"=Nan number,its type is number
+
+//type of null when coverted to number becomes 0
+
+//"33" = 33
+//"33abc" =NaN
+//true = 1
+//null =0
+//undefined = NaN
+
+// let isLoggedIn=1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 = true
+// 0 =false
+//"" = false
+// "tanya" = true
+
+
+let n=33
+let stringN=String(n)
+console.log(stringN)
+console.log(typeof stringN)
---
+++ basics/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let score=null;
+// console.log(typeof score);
+
+// let valueNumber=Number(score);
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
